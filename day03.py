c4870418ebc43703fc4dedb3c9aacdde72e0e12a
Troy Unverdruss <troy@unverdruss.net>
2023-12-19 21:29:54 -0500

    day 3 part 2

--- day03.py
+++ day03.py
@@ -1,3 +1,4 @@
+import math
 from dataclasses import dataclass
 from typing import Dict
 
@@ -38,6 +39,10 @@ def find_symbol_locations(grid: Dict[Point, str]) -> [Point]:
     return list(filter(lambda x: grid[x] not in not_symbols, grid.keys()))
 
 
+def find_gear_locations(grid: Dict[Point, str]) -> [Point]:
+    return list(filter(lambda x: grid[x] == "*", grid.keys()))
+
+
 # locations can refer to the same number!!
 def find_number_locs(grid):
     symbol_locs = find_symbol_locations(grid)
@@ -46,6 +51,16 @@ def find_number_locs(grid):
         for vec in adjacency_vectors:
             if grid[sl + vec] in numbers:
                 numbers_locs.append(sl + vec)
+    return numbers_locs
+
+
+def find_numbers_adjacent_to_gears_locs(grid):
+    gear_locs = find_gear_locations(grid)
+    numbers_locs = []
+    for gl in gear_locs:
+        for vec in adjacency_vectors:
+            if grid[gl + vec] in numbers:
+                numbers_locs.append(gl + vec)
     return numbers_locs
 
 
@@ -89,5 +104,37 @@ def part1():
     return sum(part_numbers)
 
 
+def find_gear_ratios(grid):
+    gear_locs = find_gear_locations(grid)
+
+    gear_ratios = []
+    for gl in gear_locs:
+        numbers_locs = []
+        for vec in adjacency_vectors:
+            if grid[gl + vec] in numbers:
+                numbers_locs.append(gl + vec)
+        part_number_locs = set()
+        for num_loc in numbers_locs:
+            part_number_loc = number_loc_to_part_number_loc(grid, num_loc)
+            part_number_locs.add(part_number_loc)
+        if len(part_number_locs) == 2:
+            gear_ratio = math.prod(
+                map(
+                    lambda x: part_number_loc_to_part_number(grid, x),
+                    part_number_locs
+                )
+            )
+            gear_ratios.append(gear_ratio)
+
+    return gear_ratios
+
+
+def part2():
+    grid = read_grid(read_lines())
+    ratios = find_gear_ratios(grid)
+    return sum(ratios)
+
+
 if __name__ == "__main__":
     print(part1())
+    print(part2())
