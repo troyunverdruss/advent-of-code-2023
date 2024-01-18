a963ca8fcdce098bc2444ba3c73b085c2ba573a8
troy <troy@unverdruss.net>
2024-01-18 13:19:47 -0800

    took a stab at memoizing knowing it would probably not work

--- java_kotlin/src/day19/Day19.kt
+++ java_kotlin/src/day19/Day19.kt
@@ -3,6 +3,10 @@ package day19
 import java.io.File
 
 class Day19 {
+    private val finalDestinations = listOf("A", "R")
+    private var workflowMap = mapOf<String, Workflow>()
+    private val memoMap = mutableMapOf<Part, String>()
+
     fun part1(): Long {
         val rawString = File("inputs/day19.txt").readText()
         val inputParts = rawString.split("\n\n")
@@ -11,26 +15,18 @@ class Day19 {
         return solvePart1(inputParts)
     }
 
-     fun solvePart1(inputParts: List<String>): Long {
-        val workflows = inputParts.first().split("\n").map { parseWorkflowString(it) }
-        val parts = inputParts.last().split("\n").filter { it.isNotEmpty() }.map { Part.parseString(it) }
+    fun solvePart1(inputs: List<String>): Long {
+        val workflows = inputs.first().split("\n").map { parseWorkflowString(it) }
+        val parts = inputs.last().split("\n").filter { it.isNotEmpty() }.map { Part.parseString(it) }
 
-        val workflowMap = workflows.associateBy { it.name }
-
+        workflowMap = workflows.associateBy { it.name }
 
         val accepted = mutableListOf<Part>()
         val rejected = mutableListOf<Part>()
 
-        val finalDestinations = listOf("A", "R")
         parts.forEach { part ->
-            var currWorkflowName = "in"
-
-            while (!finalDestinations.contains(currWorkflowName)) {
-                val currWorkflow = workflowMap[currWorkflowName]!!
-                currWorkflowName = processRules(currWorkflow, part)
-
-            }
-            when (currWorkflowName) {
+            val result = processSinglePart(part)
+            when (result) {
                 "A" -> accepted.add(part)
                 "R" -> rejected.add(part)
                 else -> throw RuntimeException("Unknown final workflow")
@@ -38,6 +34,53 @@ class Day19 {
         }
 
         return accepted.sumOf { it.sum() }
+    }
+
+    private fun processSinglePart(
+        part: Part
+    ): String {
+        var currWorkflowName = "in"
+
+        while (!finalDestinations.contains(currWorkflowName)) {
+            val currWorkflow = workflowMap[currWorkflowName]!!
+            currWorkflowName = processRules(currWorkflow, part)
+
+        }
+        return currWorkflowName
+    }
+
+    fun solvePart2(inputs: List<String>): Long {
+        val workflows = inputs.first().split("\n").map { parseWorkflowString(it) }
+        val parts = inputs.last().split("\n").filter { it.isNotEmpty() }.map { Part.parseString(it) }
+
+        workflowMap = workflows.associateBy { it.name }
+
+        memoMap.clear()
+
+        var count: Long = 0
+        (1..4000L).forEach { x ->
+            (1..4000L).forEach { m ->
+                (1..4000L).forEach { a ->
+                    (1..4000L).forEach { s ->
+                        val result = memoProcessSinglePart(Part(x, m, a, s))
+                        if (result == "A") {
+                            count += 1
+                        }
+                    }
+                }
+            }
+        }
+
+        return count
+    }
+
+    private fun memoProcessSinglePart(part: Part): String {
+        if (memoMap.containsKey(part)) {
+            return memoMap[part]!!
+        }
+        val result = processSinglePart(part)
+        memoMap[part] = result
+        return result
     }
 
     private fun processRules(workflow: Workflow, part: Part): String {
