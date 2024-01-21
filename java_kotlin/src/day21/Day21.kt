6f97727e86b3855f9ccf0261d3f68a37b96276a4
troy <troy@unverdruss.net>
2024-01-20 17:55:38 -0800

    working algo to find min steps to a point, and rough idea of modding remainder seems to work for calculating the right sums

--- java_kotlin/src/day21/Day21.kt
+++ java_kotlin/src/day21/Day21.kt
@@ -4,6 +4,7 @@ import day16.Day16.Companion.parseLinesToGrid
 import day16.Direction
 import day16.Point
 import java.io.File
+import java.util.LinkedList
 import kotlin.math.abs
 
 class Day21 {
@@ -13,16 +14,61 @@ class Day21 {
     }
 
     fun solvePart1(lines: List<String>, targetSteps: Long): Long {
-        grid = parseLinesToGrid(lines)
-        max = Point(
-            grid.maxBy { it.key.x }.key.x,
-            grid.maxBy { it.key.y }.key.y
-        )
+        initGlobals(lines)
         val start = grid.filter { it.value == "S" }.map { it.key }.first()
         return find(targetSteps, start, 0).size.toLong()
     }
 
-    fun find(targetSteps: Long, loc: Point, steps: Long): Map<Point, Long> {
+    fun solvePart1Take2(lines: List<String>, targetSteps: Long): Long {
+        initGlobals(lines)
+        val start = grid.filter { it.value == "S" }.map { it.key }.first()
+        return grid
+            .filter { it.value != "#" }
+            .map { minStepsToPoint(start, it.key) }
+            .filter { it <= targetSteps }
+            .map { (targetSteps - it) % 2 }
+            .count { it == 0L }
+            .toLong()
+    }
+
+
+    fun minStepsToPoint(start: Point, dest: Point): Long {
+        if (start == dest) {
+            return 0
+        }
+        val visited = HashSet<Node>()
+        val toVisit = LinkedList<Node>()
+        toVisit.add(Node(start))
+        while (toVisit.isNotEmpty()) {
+            val currNode = toVisit.poll()!!
+            if (currNode.loc == dest) {
+                var steps = 0
+                var prevNode: Node? = currNode.prevNode
+                while (prevNode != null) {
+                    steps += 1
+                    prevNode = prevNode.prevNode
+                }
+                return steps.toLong()
+            }
+            visited.add(currNode)
+            Direction.entries.forEach { dir ->
+                val nextPoint = currNode.loc + dir.point
+                getFromRepeatingGrid(nextPoint).let {
+                    if (it != "#") {
+                        val toVisitNode = Node(nextPoint)
+                        toVisitNode.prevNode = currNode
+                        if (!toVisit.contains(toVisitNode)) {
+                            toVisit.add(toVisitNode)
+                        }
+                    }
+                }
+            }
+        }
+        throw RuntimeException("Could not find path to node")
+    }
+
+
+    private fun find(targetSteps: Long, loc: Point, steps: Long): Map<Point, Long> {
         val key = MemoKey(targetSteps, loc, steps)
         memoMap[key]?.let {
             return it
@@ -35,21 +81,21 @@ class Day21 {
             return result
         }
 
-        val result = Direction.values().flatMap { dir ->
+        val result = Direction.entries.flatMap { dir ->
             val nextPoint = loc + dir.point
             val nextPointLookup = Point(
                 boundLookup(max.x, nextPoint.x),
                 boundLookup(max.y, nextPoint.y)
             )
 
-            grid[nextPointLookup]?.let { gridValue ->
-                if (gridValue != "#") {
-                    listOf(find(targetSteps, nextPoint, steps + 1))
-                } else {
-                    listOf(mapOf())
+            getFromRepeatingGrid(nextPoint)
+                .let { gridValue ->
+                    if (gridValue != "#") {
+                        listOf(find(targetSteps, nextPoint, steps + 1))
+                    } else {
+                        listOf(mapOf())
+                    }
                 }
-            }
-                ?: throw RuntimeException()
         }
 
         val resultMap = mutableMapOf<Point, Long>()
@@ -65,21 +111,6 @@ class Day21 {
         return resultMap
     }
 
-    private fun boundLookup(maxValue: Long, value: Long): Long {
-        return if (value < 0) {
-            val offset = abs((value % (maxValue + 1)))
-            if (offset == 0L) {
-                offset
-            } else {
-                maxValue + 1 - offset
-            }
-
-        } else if (value > maxValue) {
-            (value % (maxValue + 1))
-        } else {
-            value
-        }
-    }
 
     companion object {
         var grid: Map<Point, String> = mapOf()
@@ -87,6 +118,42 @@ class Day21 {
         val memoMap: MutableMap<MemoKey, Map<Point, Long>> = mutableMapOf()
 
         data class MemoKey(val targetSteps: Long, val loc: Point, val steps: Long)
+
+        fun initGlobals(lines: List<String>) {
+            grid = parseLinesToGrid(lines)
+            max = Point(
+                grid.maxBy { it.key.x }.key.x,
+                grid.maxBy { it.key.y }.key.y
+            )
+        }
+        fun getFromRepeatingGrid(loc: Point): String {
+            val nextPointLookup = Point(
+                boundLookup(max.x, loc.x),
+                boundLookup(max.y, loc.y)
+            )
+
+            return grid[nextPointLookup]!!
+        }
+
+        private fun boundLookup(maxValue: Long, value: Long): Long {
+            return if (value < 0) {
+                val offset = abs((value % (maxValue + 1)))
+                if (offset == 0L) {
+                    offset
+                } else {
+                    maxValue + 1 - offset
+                }
+
+            } else if (value > maxValue) {
+                (value % (maxValue + 1))
+            } else {
+                value
+            }
+        }
     }
+}
+
+data class Node(val loc: Point) {
+    var prevNode: Node? = null
 }
 
--- java_kotlin/test/day21/Day21Test.kt
+++ java_kotlin/test/day21/Day21Test.kt
@@ -1,5 +1,8 @@
 package day21
 
+import day16.Day16
+import day16.Direction
+import day16.Point
 import org.testng.Assert.*
 import org.testng.annotations.Test
 
@@ -23,46 +26,79 @@ class Day21Test {
     fun `verify example 1, 1 step`() {
         val day = Day21()
         assertEquals(day.solvePart1(lines, 1), 2)
+        assertEquals(day.solvePart1Take2(lines, 1), 2)
     }
 
     @Test
     fun `verify example 1, 2 steps`() {
         val day = Day21()
         assertEquals(day.solvePart1(lines, 2), 4)
+        assertEquals(day.solvePart1Take2(lines, 2), 4)
     }
 
     @Test
     fun `verify example 1, 3 steps`() {
         val day = Day21()
         assertEquals(day.solvePart1(lines, 3), 6)
+        assertEquals(day.solvePart1Take2(lines, 3), 6)
     }
 
     @Test
     fun `verify example 1, 6 steps`() {
         val day = Day21()
         assertEquals(day.solvePart1(lines, 6), 16)
+        assertEquals(day.solvePart1Take2(lines, 6), 16)
     }
 
     @Test
     fun `verify example 2, 6 steps`() {
         val day = Day21()
-        assertEquals(day.solvePart1(lines, 6), 16)
+        assertEquals(day.solvePart1Take2(lines, 6), 16)
     }
 
     @Test
     fun `verify example 2, 10 steps`() {
         val day = Day21()
-        assertEquals(day.solvePart1(lines, 10), 50)
+        assertEquals(day.solvePart1Take2(lines, 10), 50)
     }
 
     @Test
     fun `verify example 2, 50 steps`() {
         val day = Day21()
-        assertEquals(day.solvePart1(lines, 50), 1594)
+        assertEquals(day.solvePart1Take2(lines, 50), 1594)
     }
+
     @Test
     fun `verify example 2, 100 steps`() {
         val day = Day21()
-        assertEquals(day.solvePart1(lines, 100), 6536)
+        assertEquals(day.solvePart1Take2(lines, 100), 6536)
+    }
+
+    @Test
+    fun `verify minStepsToPointWorks`() {
+        val day = Day21()
+        Day21.initGlobals(lines)
+        val start = Day21.grid.filter { it.value == "S" }.map { it.key }.first()
+        assertEquals(day.minStepsToPoint(start, start), 0)
+        assertEquals(day.minStepsToPoint(start, start + Direction.Up.point), 1)
+        assertEquals(day.minStepsToPoint(start, start + Direction.Left.point), 1)
+
+        assertEquals(day.minStepsToPoint(start, start + Direction.Left.point + Direction.Left.point), 2)
+        assertEquals(
+            day.minStepsToPoint(
+                start,
+                start + Direction.Left.point + Direction.Left.point + Direction.Up.point
+            ),
+            3
+        )
+    }
+
+    @Test
+    fun `count steps to a point off the original grid`() {
+        val day = Day21()
+        Day21.initGlobals(lines)
+        val start = Day21.grid.filter { it.value == "S" }.map { it.key }.first()
+        assertEquals(day.minStepsToPoint(start, Point(-1,4)), 7)
+
     }
 }
